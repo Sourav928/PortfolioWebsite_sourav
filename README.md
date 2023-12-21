db16454196eb44a141f780c05a4766a7b84fd90f
Sourav928 <khandekarsourav92@gmail.com>
2023-12-22 02:02:31 +0530

    First commit

---
+++ README.md
@@ -0,0 +1,50 @@
+<h2 align="center">
+  Portfolio Website
+</h2>
+<div align="center">
+  <img alt="Demo" src="./Images/readme-img1.png" />
+</div>
+
+## Built With
+
+A personal portfolio website that features projects, a resume, and technical skills.<br/>
+
+This project was built using these technologies.
+
+- React.js
+- Node.js
+- Express.js
+- CSS3
+- VsCode
+- Vercel
+
+## Features
+
+**📖 Multi-Page Layout**
+
+**🎨 Styled with React-Bootstrap and CSS with easy-to-customize colors**
+
+**📱 Fully Responsive**
+
+## Getting Started
+
+Clone down this repository. You will need `node.js` and `git` installed globally on your machine.
+
+## 🛠 Installation and Setup Instructions
+
+1. Installation: `npm install`
+
+2. In the project directory, you can run: `npm start`
+
+Runs the app in the development mode.\
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+The page will reload if you make edits.
+
+## Usage Instructions
+
+Open the project folder and navigate to `/src/components/`. <br/>
+You will find all the components used, and you can edit your information accordingly.
+
+### Show your support
+
+Give a ⭐ if you like this website!
---
+++ src/components/Projects/Projects.js
@@ -0,0 +1,104 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import ProjectCard from "./ProjectCards";
+import Particle from "../Particle";
+import leaf from "../../Assets/Projects/leaf.png";
+import emotion from "../../Assets/Projects/emotion.png";
+import editor from "../../Assets/Projects/codeEditor.png";
+import cash from "../../Assets/cash.png";
+import job from "../../Assets/job.png";
+import coin from "../../Assets/coin.png";
+import portfolio from "../../Assets/portfolio.png";
+import ats from "../../Assets/ats.png";
+import SimonGame from "../../Assets/SimonGame.jpg";
+import HexCode from "../../Assets/HexCode.jpg";
+
+import chatify from "../../Assets/Projects/chatify.png";
+import suicide from "../../Assets/Projects/suicide.png";
+import bitsOfCode from "../../Assets/Projects/blog.png";
+
+function Projects() {
+  return (
+    <Container fluid className="project-section">
+      <Particle />
+      <Container>
+        <h1 className="project-heading">
+          My Recent <strong className="purple">Works </strong>
+        </h1>
+        <p style={{ color: "white" }}>
+          Here are a few projects I've worked on recently.
+        </p>
+        <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={cash}
+              isBlog={false}
+              title="Baat-Cheet"
+              description=" Developed a real-time chat application to facilitate a seamless and interactive chatting experience with features for text-based messaging and real-time message updates. Key features of the project include real-time chat functionality, user authentication, the ability to send and
+receive messages instantly with Socket.io, and message time using timeago.js"
+              ghLink="https://github.com/Sourav928/Baat-Cheet"
+              // demoLink="https://cash-floww.vercel.app/"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={job}
+              isBlog={false}
+              title="Food-Box"
+              description="Developed a feature-rich food delivery application, provides a convenient platform for ordering and delivering.Application as features including registration and authentication, payment, and order placement."
+              ghLink="https://github.com/Sourav928/Food-Box"
+              // demoLink="https://job-brd.vercel.app/"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={coin}
+              isBlog={false}
+              title="Activity-Tracker"
+              description="Developed a user friendly platform,that help users to keep the track of their daily tasks.Key features such as responsive website along with seamless CRUD operations.User friendly website is build with ReactJS, powered by NodeJS server,Express in backend with
+MongoDB database"
+              // ghLink="https://github.com/Sourav928/Activity_Tracker"
+              // demoLink="https://coin-stats.vercel.app/"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={SimonGame}
+              isBlog={false}
+              title="Simon Game"
+              description="Developed a sophisticated simulation of the classic Simon game by leveraging a combination of jQuery, HTML, CSS, and JavaScript. The implementation showcases a mastery of front-end technologies, ensuring a seamless and engaging user experience."
+              ghLink="https://github.com/Sourav928/SimonGame_Simulation"
+              demoLink="https://stirring-liger-127a1a.netlify.app"
+            />
+          </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={portfolio}
+              isBlog={false}
+              title="Portfolio"
+              description="My portfolio website is a one-stop platform displaying my projects, GitHub contributions, tech stack, resume, blog, and contact information. It encapsulates my journey as a software engineer, making it a valuable resource for potential employers and collaborators.This website serves as a dynamic tool for networking, job opportunities, and collaboration, reflecting my passion and proficiency as a software engineer."
+              ghLink="https://github.com/Sourav928/Sourix_Portfolio"
+              // demoLink="https://sushrut.vercel.app/"
+            />
+          </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={HexCode}
+              isBlog={false}
+              title="HeX-ColorCode-React"
+              description="
+Engineered a React application for the generation of hexadecimal color codes, utilizing fundamental React hooks, such as useState and useEffect.The Hexcode generator underscores a commitment to leveraging foundational React concepts for practical and user-centric applications."
+              ghLink="https://github.com/Sourav928/HexColorCode-React"
+              demoLink="https://shimmering-fairy-c5c39e.netlify.app"
+            />
+          </Col>
+        </Row>
+      </Container>
+    </Container>
+  );
+}
+
+export default Projects;
